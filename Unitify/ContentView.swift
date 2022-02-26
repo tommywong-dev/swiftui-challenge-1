9ca99a1ab87422923b1a83ff77b6a72592d33666
Tommy Wong <tommywong.dev@gmail.com>
2022-02-26 21:46:56 +0800

    Initial Commit

---
+++ Unitify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unitify
+//
+//  Created by Tommy Wong on 26/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unitify/UnitifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitifyApp.swift
+//  Unitify
+//
+//  Created by Tommy Wong on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
